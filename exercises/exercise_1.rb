7de5f9ba2c26923ac128dcf52c6d8fec753d947b
Jerome Garcia <jeromeo.garcia@gmail.com>
2024-02-17 11:35:44 -0700

    excercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,16 @@
 require_relative '../setup'
+require_relative './exercise_1'
 
-puts "Exercise 1"
+puts "Exercise 2"
 puts "----------"
 
-# Your code goes below here ...
+# Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+
+p @store1
+
+@store2 = Store.find_by(id: 2)
+
+@store1.annual_revenue = 35000
+@store1.save
